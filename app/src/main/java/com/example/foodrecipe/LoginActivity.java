421fd58a775db0a065d42728fc6daba959bd0a1d
Kub_Mite <u.etimbuk333@gmail.com>
2019-09-05 14:10:12 +0100

    Simple Example using Retrofit to call data from API

--- app/src/main/java/com/example/foodrecipe/LoginActivity.java
+++ app/src/main/java/com/example/foodrecipe/LoginActivity.java
@@ -5,51 +5,91 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.foodrecipe.model.RecipeList;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
 
-    RelativeLayout mLayout;
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                  //  "(?=.*[a-zA-Z])" +      //any letter
+                    // "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{6,}" +               //at least 4 characters
+                    "$");
+//    private static final Pattern EMAIL_PATTERN =
+//            Pattern.compile("^" +
+//                    "(?=.*[0-9])" +         //at least 1 digit
+//                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+//                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+//                    "(?=.*[a-zA-Z])" +      //any letter
+//                    // "(?=.*[@#$%^&+=])" +    //at least 1 special character
+//                    "(?=\\S+$)" +           //no white spaces
+//                    ".{4,}" +               //at least 4 characters
+//                    "$");
 
-    public EditText email_et;
-    public EditText password_et;
-    public Button login_btn;
-
+    public EditText textInputEmail;
+    public EditText textInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mLayout = findViewById(R.id.Login_page);
-        email_et = findViewById(R.id.editText_email);
-        password_et = findViewById(R.id.editText_password);
-        login_btn = findViewById(R.id.button_login);
+        textInputEmail = findViewById(R.id.editText_email);
+        textInputPassword = findViewById(R.id.editText_password);
 
 
-
-
-
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                validation(email_et.getText().toString(), password_et.getText().toString() );
-            }
-        });
     }
 
-    public void validation(String email_et, String password_et){
-        if (email_et.equals("biggiedaddie@gbokocho.com") && password_et.equals("1234")){
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class );
-            startActivity(intent);
+    private boolean validateEmail() {
+        String emailInput = textInputEmail.getText().toString().trim();
+
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            textInputEmail.setError("Please enter a valid email address");
+            return false;
+        } else {
+            textInputEmail.setError(null);
+            return true;
         }
     }
+
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Field can't be empty");
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            textInputPassword.setError("Password too weak.Password must be 6 letter");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
+        }
     }
 
+    public void confirmInput(View v) {
+        if (!validateEmail() || !validatePassword()) {
 
+        } else {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+        }
+
+    }
+}
